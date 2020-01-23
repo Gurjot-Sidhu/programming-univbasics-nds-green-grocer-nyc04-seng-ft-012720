c0c839e7ea6f0d8b220e7b900339e425677c8a85
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 01:50:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -84,5 +84,17 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  
+  consolidated_cart = consolidate_cart(cart)
+  coupons_applied_cart = apply_coupons(consolidated_cart, coupons)
+  clerance_applied_cart = apply_clearance(coupons_applied_cart)
+  total = 0 
+  i = 0 
+  while i < clerance_applied_cart.length do 
+    total += (clerance_applied_cart[i][:count] * clerance_applied_cart[i][:price])
+    i += 1 
+  end
+  if total > 100 
+    total *= 0.9 
+  end
+  return total.round(2)
 end
